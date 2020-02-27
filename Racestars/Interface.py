f0600cdd33e18f1ff416c70e7d6cd97c5104edca
Daanedwin <Daan.aalders@hotmail.com>
2020-02-27 22:20:18 +0100

    First interface start
    Basic testing of the interface

--- Racestars/Interface.py
+++ Racestars/Interface.py
@@ -1,39 +1,59 @@
 from tkinter import *
-
 import tkinter as tk
+driversss = ["Driver", "Team","Points", "Reserve", "Fastest Laps", "Podia", "Wins", "Points Finish"]
 
 class Racestars(Frame):
     def __init__(self, master=None):
         Frame.__init__(self,master)
+        self.drivers = LabelFrame(root, text = "Drivers")
+        self.constructor = LabelFrame(root, text = "Constructor")
+        self.drivers.grid(row = 0, column = 0, sticky = N)
+        self.constructor.grid(row = 0, column = 2, sticky = N)
+    
+        # Making of buttons
 
-    def gridMaking(self, height, width, offset):
+        quiting = tk.Button(root, text="Exit", command = self.Exit)
+        quiting.grid(row = 99, column = 1)
+
+    def Exit(self): 
+        root.destroy()
+
+    def gridMaking(self, height, width, offset, option):
+        h = 0
         height = int(height)
         width = int(width)
         offset = int(offset)
         for i in range(height):
             for j in range(width):
-                w = tk.Label(root, text="Test")
-                w.grid(row=i, column=j+offset)
+                if option == "drivers":
+                    if i == 0:
+                        global driversss
+                        w = tk.Label(self.drivers, text=driversss[h])
+                    else:
+                        
+                        w = tk.Label(self.drivers, text="test")
+                    w.grid(row=i, column=j+offset)
+                    h += 1
+                elif option == "constructor":
+                    w = tk.Label(self.constructor, text="Test")
+                    w.grid(row=i, column=j+offset)
+                else:
+                    print("Not a labelframe.")
 
-    def emptyGrid(self, height, width, offset):
-        height = int(height)
-        width = int(width)
-        offset = int(offset)
-        for i in range(height):
-            for j in range(width):
-                w = tk.Label(root, text="1111")
-                w.grid(row=i, column=j+offset)
 
-
-
-
+# Setting up main frame
 root = Tk()
 Race = Racestars(master=root)
 Race.master.title("Racestars")
 Race.master.minsize(800, 400)
-Race.gridMaking(31,8,0)
-Race.emptyGrid(1,2,8)
-Race.gridMaking(11,3,10)
+
+# Setting up the grid for drivers
+
+Race.gridMaking(31,8,0, "drivers")
+
+Race.gridMaking(11,4,10, "constructor")
+
+# Starting the interface
 
 Race.mainloop()
 root.destroy()
--- Racestars/Storage.py
+++ Racestars/Storage.py
@@ -1,3 +1,9 @@
+# Module which takes car of storage
+
+# Importing Python modules
+import json
+
+# Importing self made python scripts
 import Drivers
 
 def readText(filename):
@@ -18,5 +24,31 @@ def writeText(data):
         outfile.write("\n")
         
 
-writeText(Drivers.driverSetup("Daanedwin", 7, 5, False))
-readText("Data.txt")
+#writeText(Drivers.driverSetup("Daanedwin", 7, 5, False))
+#readText("Data.txt")
+
+
+def readJson(filename):
+    data = []
+    with open(filename, "r") as infile:
+        for lines in infile:
+            data.append(lines)
+        return data
+
+def writeJson(data, files):
+    if files == "Driver":
+        with open("Driver.json", "a") as outfile:
+            data = json.dumps(data)
+            json.dump(data, outfile, indent=2)
+    elif files == "Constructor":
+        with open("Constructor.json") as outfile:
+            pass
+
+    else:
+        print("Invalid file")
+
+
+
+writeJson(Drivers.driverSetup("Daanedwin", 7, 5, False), "Driver")
+s = (readJson("Driver.json"))
+print(s[0])
